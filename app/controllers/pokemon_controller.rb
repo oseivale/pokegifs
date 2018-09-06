332c43e095336fa4cfe6a5d1cd8787ca25dde110
Valerie Osei <valerie.osei@gmail.com>
2018-09-06 17:28:18 -0400

    completed pokemon api

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,6 +1,33 @@
 class PokemonController < ApplicationController
 
   def index
-    
+    # #render json: { "message": "ok" }
+    # res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/")
+    # body = JSON.parse(res.body)
+    # puts body["name"] # should be "pikachu"
+    # res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=pikachu&rating=g")
+    #
+    # body = JSON.parse(res.body)
+    #
+    # render json: {"name": body["name"], "id": body["id"], "types": body["types"], "gif": }
+
+
+
+  end
+
+  def show
+    @id = params[:id]
+    res = HTTParty.get("http://pokeapi.co/api/v2/pokemon/#{@id}")
+    body = JSON.parse(res.body)
+    # render json: {"id": @id, "name": body["name"], "types": body["types"]}
+
+      # gif_res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{body["name"]}&rating=g")
+      puts ENV["GIPHY_KEY"]
+          gif_res = HTTParty.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}=#{body["name"]}&rating=g")
+
+      gif_body = JSON.parse(gif_res.body)
+      puts gif_body
+      url = gif_body["data"][0]["url"]
+      render json: {"id": body["id"], "name": body["name"], "types": body["types"], "gif": url}
   end
 end
